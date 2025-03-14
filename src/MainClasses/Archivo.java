24a25cd7d82c00fe81053748feef2d33da502ef0
David Mavares <david.mavares@correo.unimet.edu.ve>
2025-03-14 19:37:17 -0400

    Primeras clases principales

---
+++ src/MainClasses/Archivo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainClasses;
+
+import EDD.Lista;
+
+/**
+ *
+ * @author david
+ */
+public class Archivo {
+    public String nombre;
+    public int tamano; // Número de bloques asignados
+    public Lista listaBloques; // Lista enlazada de bloques del archivo
+    public Archivo siguiente; // Referencia al siguiente archivo en la lista
+
+    public Archivo(String nombre, int tamano) {
+        this.nombre = nombre;
+        this.tamano = tamano;
+        this.listaBloques = new Lista(); // Inicializar lista de bloques
+        this.siguiente = null; // Por defecto, no tiene enlace con otro archivo
+    }
+
+
+}
---
+++ src/MainClasses/Block.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainClasses;
+
+/**
+ *
+ * @author david
+ */
+public class Block {
+    public int id;          // Identificador del bloque (posición en el array)
+    public Block siguiente;  // Referencia directa al siguiente bloque (null si es el último)
+    public boolean ocupado; // Estado del bloque (true = usado, false = libre)
+
+    public Block(int id) {
+        this.id = id;
+        this.siguiente = null;  // Por defecto, no tiene enlace
+        this.ocupado = false;
+}
+}
